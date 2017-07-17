60cd68cacd331b150d4dd3a079e6603c65f790a6
Susan Feng <susan.feng.1@gmail.com>
2017-07-16 20:58:07 -0400

    Add zoom-shrink effect to point balance on click

--- src/components/PointBalance.js
+++ src/components/PointBalance.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import accounting from 'accounting'
 
+function glowText(event) {
+  var element = event.currentTarget;
+  event.currentTarget.className = "point-text glow-up glow";
+  setTimeout(function () {
+    this.className = "point-text glow-up";
+  }.bind(element), 1000);
+}
+
+function removeGlow(event) {
+  event.currentTarget.className = "point-text glow-up";
+}
+
 const PointBalance = ({ amount }) => (
+
   <div style={{ marginTop: 30, marginBottom: 45 }}>
-    <p className="point-text">
-      {accounting.formatNumber(amount)}
+    <p>
+      <span className="point-text glow-up" 
+        onClick={event => glowText(event)}>
+        {accounting.formatNumber(amount)}
+      </span>
     </p>
     <span
       className="label"
